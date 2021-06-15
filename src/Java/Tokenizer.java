dc2d292353e2da009cc20a11f8a86c29ca3ce045
JC-ProgJava <61588096+JC-ProgJava@users.noreply.github.com>
2021-06-15 13:38:41 +0800

    Update Tokenizer.java

--- src/Java/Tokenizer.java
+++ src/Java/Tokenizer.java
@@ -314,7 +314,7 @@ public class Tokenizer {
   }
 
   private void string() {
-    while (!isAtEnd() && (peek() != '"' || sourceCode.charAt(current) == '\\')) {
+    while (!isAtEnd() && (peek() != '"' || (sourceCode.charAt(current) == '\\') && sourceCode.charAt(current - 1) != '\\')) {
       col++;
       current++;
     }
